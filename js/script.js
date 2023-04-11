0b876bbbe7e4c9bde920d53583bfd58b089eae49
federiconl <fnl1998@hotmail.com>
2023-04-11 19:10:30 +0200

    entrega2

--- js/script.js
+++ js/script.js
@@ -1,66 +1,76 @@
 class ProductManager {
     static ultimo_id = 0;
-    
-    constructor (){
+
+    constructor() {
         this.Products = [];
     }
-    
+
     getProducts() {
         console.log(this.Products)
     }
-    
-    addProduct( title, description, price, thumbnail, code, stock){
-    ProductManager.ultimo_id = ProductManager.ultimo_id + 1;
-    
-    const newPoduct = {
-        id: ProductManager.ultimo_id,
-        title : title,
-        description : description,
-        price: price,
-        thumbnail: thumbnail,
-        code: code,
-        stock: stock
-    }
-    this.Products.push(newPoduct)
+
+    addProduct(title, description, price, thumbnail, code, stock) {
+        ProductManager.ultimo_id = ProductManager.ultimo_id + 1;
+
+        const newPoduct = {
+            id: ProductManager.ultimo_id,
+            title: title,
+            description: description,
+            price: price,
+            thumbnail: thumbnail,
+            code: code,
+            stock: stock
+        }
+        this.Products.push(newPoduct)
     }
 
-    
-    
+    getProductById(id) {
+        const getProductById = this.Products.filter(product => product.id === id)
+
+        if (getProductById.length === 0) {
+            console.log(" No hay ningun producto con este ID ")
+        }
+        else {
+            console.log(getProductById)
+        }
+    }
+
+    updateProduct(id,prop, newValue ) {
+       const findElement = this.Products.findIndex ((product)=>{
+        return product.id === id
+       });
+
+       this.Products[findElement][prop] = newValue;
+
+       console.log (this.Products)
+
+
+    }
+
+
+    deleteProduct(id) {
+        const newArray = this.Products.filter(product => product.id !== id)
+
+        console.log(newArray);
+    }
+
+
+
+   
+
 }
+
+
+
+
 
 //agregar y mostrar productos//
 const manager = new ProductManager();
-manager.addProduct('Lapiz','Para escribir',25,"./img/lapiz.jpg",'AXA1',45 );
-manager.addProduct('Borrador','Para Borrar', 15, "./img/borrador.jpg", 'AXX2', 57)
+manager.addProduct('Lapiz', 'Para escribir', 25, "./img/lapiz.jpg", 'AXA1', 45);
+manager.addProduct('Borrador', 'Para Borrar', 15, "./img/borrador.jpg", 'AXX2', 57)
 manager.addProduct('Cuaderno', 'Donde anotar todo lo que necesitas', 135, "./img/cuaderno.jpg", "AAWQ3")
+manager.getProductById(3);
 manager.getProducts();
+manager.deleteProduct(2);
+manager.updateProduct(1,"title", "Pencil")
 
-// filtrado por ID//
-const idBuscado = parseInt( 2 );
-const getProductById = manager.Products.filter(product => product.id === idBuscado)
-
-if (getProductById.length === 0 ){
-    console.log( " No hay ningun producto con este ID ")
-} else {
-    console.log( getProductById )
-}
-
-//Actualizar al array//
-
-const updateProduct = manager.Products.map (product => {
-    if (product.id === 1){
-        product.price = 15 ;
-        product.stock = 10 ;
-    }
-    return product
-} )
-
-console.log(updateProduct)
-
-// Eliminar un producto//
-
-const idToDelete = parseInt(2);
-
-const newArray = manager.Products.filter(product => product.id !== idToDelete)
-
-console.log (newArray);
